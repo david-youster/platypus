63e9e2d1f81e4b9d6f49e74c1d6047a509cd7f36
David Youster <davidyouster@gmail.com>
2016-03-13 14:54:30 +0000

    Remove annotations.

--- util.py
+++ util.py
@@ -2,13 +2,12 @@ import uuid
 import hashlib
 
 
-def generate_salt() -> str:
+def generate_salt():
     """Generate a random password salt."""
     return uuid.uuid4().hex
 
 
-def generate_password_hash(password:str, salt:str) -> str:
+def generate_password_hash(password, salt):
     """Generate a password hash using the provided salt."""
     salted_password = (password + salt).encode('utf-8')
     return hashlib.sha512(salted_password).hexdigest()
-
