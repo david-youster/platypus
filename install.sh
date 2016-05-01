8b71a1639c36aad39415042eda314b230acfd714
David Youster <davidyouster@gmail.com>
2016-05-01 20:28:42 +0100

    Add install script.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install dependencies either into the virtual environment, or locally for the user
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    args='--user'
+else
+    args=''
+fi
+
+pip3 install $args flask flask-assets markdown bleach sqlalchemy sqlalchemy_utils
+
