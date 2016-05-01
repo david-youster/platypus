bc26b6e5f1de80a079cf73e96e1f327332e7e0c9
David Youster <davidyouster@gmail.com>
2016-05-01 17:48:16 +0100

    Read port number in setup script.

--- setup.py
+++ setup.py
@@ -1,21 +1,42 @@
 from util import generate_secret_key, save_config_file
 import json
 
-config = {'title': None, 'secret_key': None, 'dev': True}
+config = {
+    'title': None,
+    'secret_key': None,
+    'dev': True,
+    'port': 5000}
 
 
 def main():
     init_project()
     save_config_file(config)
-    print('Finished setting up project. To start web application, run: ')
+    print('Finished setting up project. To start application, run: ')
     print('python3 webapp.py')
 
 def init_project():
     print('Initialising a new project...')
-    while not config['title']:
-        config['title'] = input('Enter a project title: ')
+    populate_config_fields()
     print('Creating a secret key...')
     config['secret_key'] = generate_secret_key()
+
+
+def populate_config_fields():
+    read_title()
+    read_port_number()
+
+
+def read_title():
+    while not config['title']:
+        config['title'] = input('Enter a project title: ')
+
+
+def read_port_number():
+    port = input('Enter the port number (default {}): '.format(config['port']))
+    if port.isnumeric():
+        config['port'] = port
+    else:
+        print('Invalid port number entered. The default will be used.')
 
 
 if __name__ == '__main__':
